971fc642c24ac3045d348e218a89c0096c47105e
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-03-17 22:25:57 +0900

    [Fix] ファイル構造を修正

--- js/app.js
+++
@@ -1,1 +0,0 @@
-alert("Hello, Phina.js!")
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  entry: "./js/app.js",
+  entry: "./js/script.js",
   output: {
     filename: "bundle.js",
     path: __dirname + "/build"
